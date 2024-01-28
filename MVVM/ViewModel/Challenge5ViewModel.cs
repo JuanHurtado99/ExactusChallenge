d7e06ef0c3888b12509a42839c1fcd3d11963089
Juan Pablo Hurtado Rodriguez <vaquero.h919191@outlook.com>
2024-01-28 15:01:04 -0600

    added pending challenges files and nav

---
+++ MVVM/ViewModel/Challenge5ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExactusCodeChallenge.MVVM.ViewModel
+{
+    internal class Challenge5ViewModel
+    {
+    }
+}
