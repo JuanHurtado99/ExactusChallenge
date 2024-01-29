2a370278acd2bcbdc5a2aa798cc191b700049ead
Juan Pablo Hurtado Rodriguez <vaquero.h919191@outlook.com>
2024-01-28 20:39:51 -0600

    implemented sortFriendsLocation to handle logic

--- MVVM/ViewModel/Challenge5ViewModel.cs
+++ MVVM/ViewModel/Challenge5ViewModel.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExactusCodeChallenge.Core;
+using ExactusCodeChallenge.Subroutines;
 
 namespace ExactusCodeChallenge.MVVM.ViewModel
 {
-    internal class Challenge5ViewModel
+    class Challenge5ViewModel : ObservableObject
     {
+        private FriendsNavigation _friendsNavigation;
+
+        private List<int[]> _friendsSorted;
+        private string _friendsString;
+        public string friendsString
+        {
+            get 
+            {
+                return _friendsString;
+            }
+            set
+            {
+                _friendsString = value;
+                OnPropertyChanged();
+            }
+        }
+        public Challenge5ViewModel()
+        {
+            int[] startingPoint = [10,15];
+            int[][] friendsLocations = [[10,20],[20,20],[10,10],[100,10]];
+            _friendsNavigation = new FriendsNavigation();
+            _friendsSorted = _friendsNavigation.sortFriendsLocation(startingPoint, friendsLocations);
+            _friendsString = friendsResultToString();
+            
+        }
+
+        private string friendsResultToString()
+        {
+            var result = "";
+            foreach (var friend in _friendsSorted) 
+            {
+                var friendString = "[ " + friend[0].ToString() + " , "+friend[1].ToString() + " ]\n";
+                result += friendString;
+            }
+            return result;
+        }
     }
 }
---
+++ Subroutines/FriendsNavigation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ExactusCodeChallenge.Subroutines
+{
+    public class FriendsNavigation
+    {
+        public FriendsNavigation() { }
+
+        public List<int[]> sortFriendsLocation(int[] startingPoint, int[][] friendsLocations)
+        {
+            List<int[]> result = new List<int[]>();
+
+            int startingX = startingPoint[0];
+            int startingY = startingPoint[1];
+
+            List<FriendsDistance> Distances = new List<FriendsDistance>();
+
+
+            for (int i = 0; i < friendsLocations.Length; i++)
+            {
+                var distance = Math.Sqrt(Math.Pow(startingX - friendsLocations[i][0], 2) + Math.Pow(startingY - friendsLocations[i][1], 2));
+                Distances.Add(new FriendsDistance(i, distance));
+            }
+
+            Distances.Sort((x, y) => x.distance.CompareTo(y.distance));
+
+            foreach (FriendsDistance friend in Distances)
+            {
+                result.Add(friendsLocations[friend.index]);
+            }
+
+            return result;
+        }
+        private class FriendsDistance
+        {
+            public int index;
+            public double distance;
+            public FriendsDistance(int index, double distance)
+            {
+                this.index = index;
+                this.distance = distance;
+            }
+        }
+    }
+
+   
+}
